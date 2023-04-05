c8b452b3a618f671f7dab2e9fa117f65b6695590
KhoiNguyen-281 <75612025+KhoiNguyen-281@users.noreply.github.com>
2023-04-05 17:48:55 +0700

    create user collections drop down list component

---
+++ src/components/UserProfileComponents/UserProfileCollection/CollectionDropList.jsx
@@ -0,0 +1,12 @@
+
+import styles from '../../../styles/UserProfile/UserProfleCollection/CollectionDropList.module.css'
+
+const CollectionDropList = () => {
+    return(
+        <div className={`${styles.listMainContainer} ${styles.flexColumn}`}>
+            
+        </div>
+    );
+}
+
+export default CollectionDropList;
