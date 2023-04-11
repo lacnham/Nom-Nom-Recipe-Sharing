a496c49cde4cdadd8061873e461a771676f45733
KhoiNguyen-281 <75612025+KhoiNguyen-281@users.noreply.github.com>
2023-04-11 16:04:24 +0700

    fix recipeDescription origin

--- src/components/RecipeDetailPageComponents/RecipeDetailSection/RecipeDescription.jsx
+++ src/components/RecipeDetailPageComponents/RecipeDetailSection/RecipeDescription.jsx
@@ -1,28 +1,23 @@
-
 import styles from '../../../styles/RecipeDetailPage/DetailRecipePage.module.css'
 
-const RecipeDescription = (props) => {
+const RecipeDescription = props => {
+  return (
+    <>
+      <div
+        className={`${styles.recipePrimaryContainer} ${styles.boxShadowPurple}`}
+      >
+        <div className={styles.title}>Origin</div>
+        <p>{props.recipe.origin}</p>
+      </div>
+      <div
+        className={`${styles.recipePrimaryContainer} ${styles.boxShadowPurple}`}
+      >
+        {/* <div className={`${styles.authorContainer}`}>Author hehe</div> */}
+        <div className={styles.title}>Description</div>
+        <p>{props.recipe.description}</p>
+      </div>
+    </>
+  )
+}
 
-
-
-    return (
-      <>
-        <div className={`${styles.recipePrimaryContainer} ${styles.boxShadowPurple}`}>
-          <div className={styles.title}>Origin</div>
-          <p>
-            {props.recipe.origiin}
-          </p>
-        </div>
-        <div className={`${styles.recipePrimaryContainer} ${styles.boxShadowPurple}`}>
-          <div className={styles.title}>Description</div>
-          <p>
-            {props.recipe.description}
-          </p>
-        </div>
-        
-      </>
-      
-    )
-  }
-
-  export default RecipeDescription;
+export default RecipeDescription
