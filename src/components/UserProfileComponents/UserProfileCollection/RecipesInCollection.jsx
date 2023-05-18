d86b2d4586013bde3e45293a3929198bbc36087f
KhoiNguyen-281 <75612025+KhoiNguyen-281@users.noreply.github.com>
2023-05-18 18:44:23 +0700

    add routing for recipe in collection profile

--- src/components/UserProfileComponents/UserProfileCollection/RecipesInCollection.jsx
+++ src/components/UserProfileComponents/UserProfileCollection/RecipesInCollection.jsx
@@ -1,6 +1,7 @@
 import { Suspense, lazy } from 'react'
 import styles from '../../../styles/UserProfile/UserProfleCollection/CollectionRecipe.module.css'
 import { FetchCollectionRecipe } from './FetchCollectionRecipe'
+import { Link } from 'react-router-dom'
 
 export const RecipesInCollection = props => {
   const recipes = FetchCollectionRecipe(props.id)
@@ -14,13 +15,17 @@ export const RecipesInCollection = props => {
   const Card = lazy(() => import('../../Card'))
 
   const recipe = recipes.map(ele => (
-    <Card
-      image={ele.image_link}
-      title={ele.name}
-      category={['asd', 'asd1', 'asd2']}
-      location="Downtown, Seattle WA"
-      description={ele.description}
-    />
+    <Link to={`/recipe/${ele.name}/${ele.recipe_id}`} key={ele.recipe_id}>
+      <Suspense>
+        <Card
+          image={ele.image_link}
+          title={ele.name}
+          category={['asd', 'asd1', 'asd2']}
+          location="Downtown, Seattle WA"
+          description={ele.description}
+        />
+      </Suspense>
+    </Link>
   ))
 
   return (
